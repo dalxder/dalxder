ae9f239191456b3c195f089fb1d2e2f9532b4241
Dalxder <dalxder@gmail.com>
2017-07-03 08:12:16 -0500

    Add files via upload

---
+++ angular_test/js/app.js
@@ -0,0 +1,25 @@
+(function(){
+	"use strict";
+	var App=angular.module('Prueba',[
+		// dependence here
+		])
+	.controller('MainController',function($scope) {
+		$scope.your_name='you are the user';
+		$scope.categories=['SIG','Ejercicios','Aplicaciones'];
+		$scope.bookmarks=[
+		{id:1,name:'youtube.com',url:'https://youtube.com', category: 'SIG'},
+		{id:2,name:'github.com',url:'https://github.com', category: 'Aplicaciones'}
+		];
+		$scope.currentCategory='SIG';
+		$scope.setCurrentCategory=function(category){
+			$scope.currentCategory=category;
+		}
+		$scope.isCurrentCategory=function(category){
+			return $scope.currentCategory==category;
+		}
+		// body...
+	});
+	console.log(App);
+
+
+})();
